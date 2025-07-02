62d1d04b7ab78c811d524e3e20888cd760f3ea75
Arach <arach@tchoupani.com>
2025-07-01 20:08:13 -0400

    Initial Commit

---
+++ Loopa/LoopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopaApp.swift
+//  Loopa
+//
+//  Created by Arach Tchoupani on 2025-07-01.
+//
+
+import SwiftUI
+
+@main
+struct LoopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoopaTests/LoopaTests.swift
@@ -0,0 +1,16 @@
+//
+//  LoopaTests.swift
+//  LoopaTests
+//
+//  Created by Arach Tchoupani on 2025-07-01.
+//
+
+import Testing
+
+struct LoopaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
